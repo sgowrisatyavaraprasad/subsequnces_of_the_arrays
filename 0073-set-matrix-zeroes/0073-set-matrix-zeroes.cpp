0571fa117d63fb42e36e2dcf024dbd4e262b1da4
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2025-09-23 21:53:17 +0530

    Time: 119 ms (3.05%), Space: 18.6 MB (78.67%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int , int>> vec;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(matrix[i][j] == 0){
+                    vec.push_back({i , j});
+                }
+            }
+        }
+        for(auto it : vec){
+            for(int i = 0; i < matrix.size(); i++){
+                for(int j = 0; j < matrix[i].size(); j++){
+                    if(i == it.first || it.second == j){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
