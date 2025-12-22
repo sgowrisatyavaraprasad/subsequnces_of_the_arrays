891a5758933cc02b5e39d6d6a5c10a4e64034067
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2025-12-22 21:50:54 +0530

    Time: 271 ms (19%), Space: 17.3 MB (96.54%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,12 +1,13 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         nl = []
+        ps = 0
         for i in range(1 , n + 1):
             nl.append(i)
 
         for i in range(0 , len(nl)):
-            a = sum(nl[0 : i + 1])
+            ps += nl[i]
             b = sum(nl[i : len(nl)])
-            if a == b:
+            if ps == b:
                 return nl[i]
         return -1
