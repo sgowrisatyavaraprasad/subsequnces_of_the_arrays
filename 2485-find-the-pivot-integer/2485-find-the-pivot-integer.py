cb22662afbed20cee41611bd3cb3fdc3a5883df6
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2025-12-22 21:56:22 +0530

    Time: 443 ms (17.55%), Space: 17.4 MB (95.45%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -3,11 +3,8 @@ class Solution:
         nl = []
         ps = 0
         for i in range(1 , n + 1):
-            nl.append(i)
-
-        for i in range(0 , len(nl)):
-            ps += nl[i]
-            b = sum(nl[i : len(nl)])
-            if ps == b:
-                return nl[i]
+            ps += i
+            a = sum(range(i , n + 1))
+            if a == ps:
+                return i
         return -1
