f3bc4dea4534646d8da196156dc1ccc248c7ec0e
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2026-02-19 09:36:03 +0530

    Time: 415 ms (99.41%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+SELECT p.firstName , p.lastName , a.city , a.state FROM
+Person p
+LEFT JOIN 
+Address a
+ON p.personId = a.personId
+ORDER BY p.firstName;
