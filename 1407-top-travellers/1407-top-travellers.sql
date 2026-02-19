dbeafe67d5e94e41ac9f9c0fddb4c6eee0a16773
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2026-02-19 10:29:27 +0530

    Time: 914 ms (21.11%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+SELECT  u.name , NVL(SUM(r.distance) , 0 ) AS travelled_distance FROM
+Users u
+LEFT JOIN
+Rides r
+ON u.id = r.user_id
+GROUP BY  r.user_id , u.name
+
+ORDER BY NVL(SUM(r.distance) , 0) DESC , u.name ASC ;
